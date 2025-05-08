5689247e18d2ec8266639c8100f6bd08adcf40e4
thepratholic <chelaramanipratham@gmail.com>
2025-05-08 14:48:10 +0530

    App Updated

--- src/app/sorting/mergeSort.cpp
+++ src/app/sorting/mergeSort.cpp
@@ -8,5 +8,14 @@ MergeSort::MergeSort(vector<int> *nums) {
 MergeSort::~MergeSort() {}
 
 void MergeSort::recursiveSort(int left, int right) {
-    
+
+}
+
+
+void MergeSort::sort() {
+    if((*nums).size() == 0) {
+        return;
+    }
+
+    recursiveSort(0, (*nums).size());
 }
